1d45075dc591f2eed73bf14d31988b9b80e642f7
Zanele <zsp220696.sibanyoni@gmail.com>
2022-03-12 07:31:32 +0200

    add archui-components and archui-demo

---
+++ archui-components/src/button.js
@@ -0,0 +1,15 @@
+// Button.js
+import React from 'react';
+
+const Button = (props) => {
+  const { type } = props;
+  return (
+    <button
+      type={type || "submit"}
+      className="acme-button">
+      {props.children}
+    </button>
+  )
+}
+
+export default Button;
